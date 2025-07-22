55fa87e23c136a896630b9522d753b2acc565356
Drworm82 <worm_jim@hotmail.com>
2025-07-22 14:37:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.getElementById('main-header'); // Obtenemos el elemento del encabezado
+    const stickyPoint = header.offsetTop; // Obtenemos la posición inicial del encabezado
 
+    function makeHeaderSticky() {
+        if (window.scrollY > stickyPoint) {
+            // Si el scroll supera la posición inicial del encabezado, añadimos la clase 'sticky'
+            header.classList.add('sticky');
+        } else {
+            // Si el scroll está por encima, removemos la clase 'sticky'
+            header.classList.remove('sticky');
+        }
+    }
+
+    // Añadimos un "escuchador de eventos" para el scroll de la ventana
+    window.addEventListener('scroll', makeHeaderSticky);
+});
